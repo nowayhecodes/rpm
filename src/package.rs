9d1a3edf2f23b4c6a9bd56304cf2fc2e0de05b06
Gus Cavalcante <nowayhecodes@gmail.com>
2024-11-01 02:06:20 -0300

    feat: add impl of package json struct

--- src/package.rs
+++ src/package.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
 use semver::Version;
 use std::collections::HashMap;
+use tokio::fs;
+use anyhow::Result;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Package {
@@ -21,5 +23,28 @@ pub struct PackageJson {
     pub name: String,
     pub version: String,
     pub dependencies: Option<HashMap<String, String>>,
+    #[serde(rename = "devDependencies")]
     pub dev_dependencies: Option<HashMap<String, String>>,
+}
+
+impl PackageJson {
+    pub async fn load() -> Result<Self> {
+        let content = fs::read_to_string("package.json").await?;
+        Ok(serde_json::from_str(&content)?)
+    }
+
+    pub async fn save(&self) -> Result<()> {
+        let content = serde_json::to_string_pretty(self)?;
+        fs::write("package.json", content).await?;
+        Ok(())
+    }
+
+    pub fn remove_dependency(&mut self, package: &str) {
+        if let Some(deps) = &mut self.dependencies {
+            deps.remove(package);
+        }
+        if let Some(dev_deps) = &mut self.dev_dependencies {
+            dev_deps.remove(package);
+        }
+    }
 } 
